57513bb4557d42f5b0601b88954d7d3e3d7bc1f9
Jenish <jnis.mhr@gmail.com>
2025-09-18 20:13:53 +0545

    Removed Commented code

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,13 +1,4 @@
-// const { test, expect} = require('@playwright/test');
-// const { LoginPage } = require('../pageObjects/LoginPage');
-// const { validuser } = require('../fixtures/userData');
 
-// test('valid user should log in successfully', async ({ page }) => {
-//     const loginPage = new LoginPage(page);
-//     await loginPage.goto();
-//     await loginPage.login(validuser.username, validuser.password);
-//     await expect(page).toHaveURL(/dashboard/)
-// });
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pageObjects/LoginPage');
 const { validUser } = require('../fixtures/userData');
