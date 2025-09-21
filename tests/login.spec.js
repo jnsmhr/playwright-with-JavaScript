1a40bb314842766cea7eb1bdcfd699903080cd3a
Jenish <jnis.mhr@gmail.com>
2025-09-21 10:07:09 +0545

    modifed the test with test describe

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,25 +4,26 @@ const { LoginPage } = require('../pageObjects/LoginPage');
 const { validUser, emptyUser, invalidUser } = require('../fixtures/userData');
 const { DashboardPage } = require('../pageObjects/DashboardPage');
 
-test('Valid user should log in successfully', async ({ page }) => {
+test.describe('Testing Login Funcationality', ()=>{
+  
+  test.beforeEach(async ({page})=>{
+    const loginPage = new LoginPage(page);    
+    await loginPage.goto();
+  });
+test('Valid user should log in successfully', async ({ page }) => {  
   const loginPage = new LoginPage(page);
-  const dashboard = new DashboardPage(page);
-  await loginPage.goto();
+  const dashboard = new DashboardPage(page);  
   await loginPage.login(validUser.username, validUser.password);
-  const actualText = await dashboard.getTitle();
-  expect(actualText).toBe('Products');
+  await expect(dashboard.title).toHaveText('Products');
 })
 test('Testing empty user', async ({page})=>{
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
+  const loginPage = new LoginPage(page); 
   await loginPage.login(emptyUser.username, emptyUser.password);
-  const emptyValidation = await loginPage.getValidationMessage();
-  expect(emptyValidation).toBe('Epic sadface: Username is required');
+  await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username is required');
 })
-test('Invalid Login', async ({page})=>{
+test('Invalid Login', async ({page})=>{  
   const loginPage = new LoginPage(page);
-  await loginPage.goto();
   await loginPage.login(invalidUser.username, invalidUser.password);
-  const errorMessage = await loginPage.getValidationMessage();
-  expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
+  await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+})
 })
