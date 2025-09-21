94f9ac8428ce51f5874248b91c288ed6ac7c4d0c
Jenish <jnis.mhr@gmail.com>
2025-09-21 13:20:16 +0545

    added error message file

---
+++ fixtures/errors.js
@@ -0,0 +1,6 @@
+module.exports={
+    TITLE_VALIDATION: "Products",
+    EMPTY_USER: "Epic sadface: Username is required",
+    INVALID_LOGIN: "Epic sadface: Username and password do not match any user in this service",
+    EMPTY_PASSWORD: "Epic sadface: Password is required",
+}
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,29 +1,32 @@
 
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pageObjects/LoginPage');
-const { validUser, emptyUser, invalidUser } = require('../fixtures/userData');
+const { validUser, emptyUser, invalidUser, emptyPassword } = require('../fixtures/userData');
 const { DashboardPage } = require('../pageObjects/DashboardPage');
+const { TITLE_VALIDATON, EMPTY_USER, INVALID_LOGIN, EMPTY_PASSWORD, TITLE_VALIDATION } = require('../fixtures/errors');
 
-test.describe('Testing Login Funcationality', ()=>{
-  
+test.describe('Login Functionality Tests', ()=>{
+  let loginPage,dashboard
+
   test.beforeEach(async ({page})=>{
-    const loginPage = new LoginPage(page);    
+    loginPage = new LoginPage(page);
+    dashboard = new DashboardPage(page);      
     await loginPage.goto();
   });
-test('Valid user should log in successfully', async ({ page }) => {  
-  const loginPage = new LoginPage(page);
-  const dashboard = new DashboardPage(page);  
+test('Valid user should log in successfully', async ({ page }) => {   
   await loginPage.login(validUser.username, validUser.password);
-  await expect(dashboard.title).toHaveText('Products');
+  await expect(dashboard.title).toHaveText(TITLE_VALIDATION);
 })
-test('Testing empty user', async ({page})=>{
-  const loginPage = new LoginPage(page); 
+test('Empty username should show required error', async ({page})=>{
   await loginPage.login(emptyUser.username, emptyUser.password);
-  await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username is required');
+  await expect(loginPage.errorMessage).toHaveText(EMPTY_USER);
 })
-test('Invalid Login', async ({page})=>{  
-  const loginPage = new LoginPage(page);
+test('Invalid Login should show error message', async ({page})=>{  
   await loginPage.login(invalidUser.username, invalidUser.password);
-  await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+  await expect(loginPage.errorMessage).toHaveText(INVALID_LOGIN);
+})
+test('Empty Password should show required error',async({page})=>{
+  await loginPage.login(emptyPassword.username,emptyPassword.password)
+  await expect(loginPage.errorMessage).toHaveText(EMPTY_PASSWORD);
 })
 })
