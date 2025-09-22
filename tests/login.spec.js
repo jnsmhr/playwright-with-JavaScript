5979ce4c41fbd16732d03cdc14d277f837d61e3e
Jenish <jnis.mhr@gmail.com>
2025-09-22 10:13:32 +0545

    added error, userdata and fix login

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,10 +1,9 @@
 
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pageObjects/LoginPage');
-const { validUser, emptyUser, invalidUser, emptyPassword } = require('../fixtures/userData');
+const { validUser, emptyUser, invalidUser, emptyPassword, lockedUser } = require('../fixtures/userData');
 const { DashboardPage } = require('../pageObjects/DashboardPage');
-const { TITLE_VALIDATON, EMPTY_USER, INVALID_LOGIN, EMPTY_PASSWORD, TITLE_VALIDATION } = require('../fixtures/errors');
-
+const  {LOGIN, INVENTORY}  = require('../fixtures/errors');
 test.describe('Login Functionality Tests', ()=>{
   let loginPage,dashboard
 
@@ -15,18 +14,23 @@ test.describe('Login Functionality Tests', ()=>{
   });
 test('Valid user should log in successfully', async ({ page }) => {   
   await loginPage.login(validUser.username, validUser.password);
-  await expect(dashboard.title).toHaveText(TITLE_VALIDATION);
+  await expect(page).toHaveURL(/inventory.html/);
+  await expect(dashboard.title).toHaveText(INVENTORY.TITLE_VALIDATION);
 })
 test('Empty username should show required error', async ({page})=>{
   await loginPage.login(emptyUser.username, emptyUser.password);
-  await expect(loginPage.errorMessage).toHaveText(EMPTY_USER);
+  await expect(loginPage.errorMessage).toHaveText(LOGIN.EMPTY_USER);
 })
 test('Invalid Login should show error message', async ({page})=>{  
   await loginPage.login(invalidUser.username, invalidUser.password);
-  await expect(loginPage.errorMessage).toHaveText(INVALID_LOGIN);
+  await expect(loginPage.errorMessage).toHaveText(LOGIN.INVALID_LOGIN);
 })
 test('Empty Password should show required error',async({page})=>{
   await loginPage.login(emptyPassword.username,emptyPassword.password)
-  await expect(loginPage.errorMessage).toHaveText(EMPTY_PASSWORD);
+  await expect(loginPage.errorMessage).toHaveText(LOGIN.EMPTY_PASSWORD);
+})
+test('Locked out user shouldnt log in',async({page})=>{
+ await loginPage.login(lockedUser.username,lockedUser.password)
+ await expect(loginPage.errorMessage).toHaveText(LOGIN.LOCKED_USER)
 })
 })
